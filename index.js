38ae60c8614f0819b15c30e316f8c70012dbbbc3
wuway <wayivan@126.com>
2016-10-17 17:29:53 +0800

    1. 加入执行npm install代码。

--- index.js
+++ index.js
@@ -6,14 +6,14 @@
  */
 'use strict';
 
-var fs            = require( 'fs' );
-var path          = require( 'path' );
-var exec          = require( 'child_process' ).exec;
-var help          = require( "./common/help.js" );
+var fs = require( 'fs' );
+var path = require( 'path' );
+var exec = require( 'child_process' ).exec;
+var help = require( "./common/help.js" );
 var projectRename = require( "./common/project_rename" );
-var argv          = require( 'minimist' )( process.argv.slice( 2 ) );
-var debug         = true;
-var kitPath       = 'https://github.com/dmgapp/react-native-dmgapp-kit.git';
+var argv = require( 'minimist' )( process.argv.slice( 2 ) );
+var debug = true;
+var kitPath = 'https://github.com/dmgapp/react-native-dmgapp-kit.git';
 
 if ( argv[ 'h' ] ) {
   help();
@@ -35,22 +35,22 @@ switch ( commands[ 0 ] ) {
       );
       process.exit( 1 );
     } else {
-      init( commands[ 1 ] , argv.verbose , argv.version );
+      init( commands[ 1 ], argv.verbose, argv.version );
     }
     break;
   default:
     console.error(
       'Command `%s` unrecognized. ' +
-      'Did you mean to run this inside a react-native project?' ,
+      'Did you mean to run this inside a react-native project?',
       commands[ 0 ]
     );
     process.exit( 1 );
     break;
 }
 
-function init( name , verbose , rnPackage ) {
+function init( name, verbose, rnPackage ) {
   validatePackageName( name );
-  if ( fs.existsSync( name )) {
+  if ( fs.existsSync( name ) ) {
     console.log( 'Project Name has Exists!' );
     process.exit();
   } else {
@@ -62,7 +62,7 @@ function validatePackageName( name ) {
   if ( !name.match( /^[$A-Z_][0-9A-Z_$]*$/i ) ) {
     console.error(
       '"%s" is not a valid name for a project. Please use a valid identifier ' +
-      'name (alphanumeric).' ,
+      'name (alphanumeric).',
       name
     );
     process.exit( 1 );
@@ -71,7 +71,7 @@ function validatePackageName( name ) {
   if ( name === 'React' ) {
     console.error(
       '"%s" is not a valid name for a project. Please do not use the ' +
-      'reserved word "React".' ,
+      'reserved word "React".',
       name
     );
     process.exit( 1 );
@@ -82,30 +82,35 @@ function validatePackageName( name ) {
  * @param name
  */
 function createProject( name ) {
-  var root        = path.resolve( name );
+  var root = path.resolve( name );
   var projectName = path.basename( root );
 
-  exec( 'git clone ' + kitPath + root , function ( e , stdout , stderr ) {
+  exec( 'git clone ' + kitPath + root, function ( e, stdout, stderr ) {
     if ( e ) {
       console.log( stdout );
       console.error( stderr );
       console.error( 'git clone failed' );
       process.exit( 1 );
     } else {
-      projectRename.init( root , projectName );
+      installPackge();
     }
   } );
-
 }
 
-//function run(root, projectName, rnPackage) {
-//  exec('npm install --save --save-exact ' + getInstallPackage(rnPackage), function(e, stdout, stderr) {
-//    if (e) {
-//      console.log(stdout);
-//      console.error(stderr);
-//      console.error('`npm install --save --save-exact react-native` failed');
-//      process.exit(1);
-//    }
-//
-//  });
-//}
+/**
+ * 安装packge依赖包，并初始化项目
+ * @param root 项目路径
+ * @param projectName 项目名称
+ */
+function installPackge( root, projectName ) {
+  exec( 'npm install', function ( e, stdout, stderr ) {
+    if ( e ) {
+      console.log( stdout );
+      console.error( stderr );
+      console.error( 'npm install failed' );
+      process.exit( 1 );
+    } else {
+      projectRename.init( root, projectName );
+    }
+  } );
+}
